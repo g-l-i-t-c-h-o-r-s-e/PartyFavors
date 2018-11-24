2a26c19aba29c47bfca15a3c82660370ee588d09
g-l-i-t-c-h-o-r-s-e <anne.hero88@gmail.com>
2018-11-24 02:32:59 -0700

    fixed binary names

--- FilterBatch/FilterBatch3.0.sh
+++ FilterBatch/FilterBatch3.0.sh
@@ -18,10 +18,10 @@ mkdir results ; cd results ; rm *.nut ; cd ../
  filecount=$(bc <<< "$filecount"+0.0001)
 zeropad=${filecount#"."}
 
-FFMPREG -ss $seek -i $1 -f nut -s $2 -vcodec huffyuv -acodec pcm_u32le \
+ffmpeg -ss $seek -i $1 -f nut -s $2 -vcodec huffyuv -acodec pcm_u32le \
 -lavfi "[0:v]fifo,trim=start=0:duration=$3[a];[0:v]fifo,trim=start=$3:duration=$4,setpts=PTS-STARTPTS[b];[b]${array1[$i]}[c];[a][c]concat[d];[0:v]trim=start=$newstart:duration=0,setpts=PTS-STARTPTS[e];[d][e]concat[out]" \
 -lavfi "[0:a]afifo,atrim=start=0:duration=$3[a];[0:a]afifo,atrim=start=$3:duration=$4,asetpts=PTS-STARTPTS[b];[b]${array2[$i]}[c];[a][c]concat=v=0:a=1[d];[0:a]atrim=start=$newstart:duration=0,asetpts=PTS-STARTPTS[e];[d][e]concat=v=0:a=1[out2]"  \
--map "[out]" -map "[out2]" -t $add - | FFMPREG -i - -f nut -c:v huffyuv -c:a pcm_u32le -y ./results/out_$zeropad.nut
+-map "[out]" -map "[out2]" -t $add - | ffmpeg -i - -f nut -c:v huffyuv -c:a pcm_u32le -y ./results/out_$zeropad.nut
 #Do the math after each ffmpeg process so that -ss starts with 00
 seek=$(bc <<< $seek+$add) ; done
  
@@ -35,5 +35,5 @@ cd ./results
  rm ieatass.txt
  wait
   for f in ./*.nut; do echo "file '$f'" >> ieatass.txt; done
-  FFMPREG -f concat -safe 0 -i ieatass.txt -codec copy -y KMS.nut
- FFPLEY KMS.nut -af volume=0.1 ; fi
+  ffmpeg -f concat -safe 0 -i ieatass.txt -codec copy -y KMS.nut
+ ffplay KMS.nut -af volume=0.1 ; fi
