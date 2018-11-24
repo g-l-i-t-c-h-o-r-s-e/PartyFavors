421cd11882029e863a2d500274f45cb6885dc182
g-l-i-t-c-h-o-r-s-e <anne.hero88@gmail.com>
2018-11-24 01:51:24 -0700

    fixed binary names

--- Audio2Image/Audio2Image.sh
+++ Audio2Image/Audio2Image.sh
@@ -1,5 +1,5 @@
 #get duration
-duration=$(FFPROBE  -i $1 -show_entries format=duration -v quiet -of csv='p=0')
+duration=$(ffprobe  -i $1 -show_entries format=duration -v quiet -of csv='p=0')
 
 #do sum maths
 size=$(bc -l <<< "$duration"*705)
@@ -8,8 +8,8 @@ height=1000
 
 
 #COMMENCE MAGIC
-FFMPREG -i $1  -f u8 -ar 44100 -ac 2 - | FFMPREG -f rawvideo -s "$width"x"$height" -pix_fmt rgb24 -i - -y -pix_fmt rgb24 ./test.xwd
-FFPLEY -f u8 -ar 44100 -ac 2 -i ./test.xwd
+ffmpeg -i $1  -f u8 -ar 44100 -ac 2 - | ffmpeg -f rawvideo -s "$width"x"$height" -pix_fmt rgb24 -i - -y -pix_fmt rgb24 ./test.xwd
+ffplay -f u8 -ar 44100 -ac 2 -i ./test.xwd
 
 echo "$width"x"$height"
 echo $duration
