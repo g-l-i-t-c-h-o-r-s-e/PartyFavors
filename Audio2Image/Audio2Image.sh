1b8e705d25c120616dff98ea597be831b1401aae
g-l-i-t-c-h-o-r-s-e <anne.hero88@gmail.com>
2018-11-24 01:49:40 -0700

    conv. audio to image for gimp/photoshop databending
    Ayy

---
+++ Audio2Image/Audio2Image.sh
@@ -0,0 +1,16 @@
+#get duration
+duration=$(FFPROBE  -i $1 -show_entries format=duration -v quiet -of csv='p=0')
+
+#do sum maths
+size=$(bc -l <<< "$duration"*705)
+width=$(bc <<< "$size"/24)
+height=1000
+
+
+#COMMENCE MAGIC
+FFMPREG -i $1  -f u8 -ar 44100 -ac 2 - | FFMPREG -f rawvideo -s "$width"x"$height" -pix_fmt rgb24 -i - -y -pix_fmt rgb24 ./test.xwd
+FFPLEY -f u8 -ar 44100 -ac 2 -i ./test.xwd
+
+echo "$width"x"$height"
+echo $duration
+
